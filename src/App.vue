6611416b209221470cc38a05132164bf49fbb4b8
Zen1tHH2510 <yanmyoaung@hmmserver.net>
2024-02-01 17:28:13 +0630

    first commit

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <p class="hello">Hello world</p>
+  <!-- <header>
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="./assets/logo.svg"
+      width="125"
+      height="125"
+    />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main> -->
+  <SignUp />
+</template>
+
+<script>
+import SignUp from "./components/SignUp.vue";
+
+export default {
+  components: {
+    SignUp,
+  },
+};
+</script>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.hello {
+  color: black;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/SignUp.vue
@@ -0,0 +1,76 @@
+<template>
+  <form>
+    <label for="email">Email</label><br />
+    <input type="email" id="email" v-model="email" /><br />
+    <label for="username">User Name</label><br />
+    <input type="text" v-model="userName" /><br />
+    <label for="username">Password</label><br />
+    <input type="password" v-model="password" /><br />
+    <select style="margin: 20px 0px">
+      <option value="">Please Select Role</option>
+      <option value="dev">Developer</option>
+      <option value="des">Designer</option>
+    </select>
+    <br />
+    <label for="">Skills</label><br />
+    <input type="text" id v-model="skill" @keyup="addSkill" /><br />
+    <!-- <button>Reset</button> -->
+
+    <div v-for="skill in skills" :key="skill">
+      <p>{{ skill }} &#10060;</p>
+    </div>
+    <br />
+    <!-- <button :v-model="(skills = '')">Reset</button> -->
+
+    <div>
+      <input type="checkbox" v-model="accept" />
+      <label style="margin-left: 5px" for="">accept terms</label><br />
+    </div>
+    <!-- <div>
+      <div>
+        <label for="">name - 1</label>
+        <input type="checkbox" value="1" v-model="names" />
+      </div>
+      <div>
+        <label for="">name - 2</label>
+        <input type="checkbox" value="2" v-model="names" />
+      </div>
+      <div>
+        <label for="">name - 3</label>
+        <input type="checkbox" value="3" v-model="names" />
+      </div>
+    </div> -->
+
+    <!-- <p>names - {{ names.toString() }}</p> -->
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      userName: "",
+      password: "",
+      accept: false,
+      names: [],
+      skills: [],
+      skill: "",
+    };
+  },
+  methods: {
+    addSkill(e) {
+      if (e.key === " ") {
+        this.skills.push(this.skill);
+        this.skill = "";
+      }
+    },
+  },
+};
+</script>
+
+<style>
+* {
+  color: black;
+}
+</style>

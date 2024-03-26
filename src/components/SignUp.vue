74a53d475f8db188bebebd359230e54d16dc2d8d
Zenith2510 <ylay0857@gmail.com>
2024-03-26 20:51:01 +0630

    push

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,11 +1,14 @@
 <template>
-  <form>
+  <form @submit.prevent="submit">
     <label for="email">Email</label><br />
     <input type="email" id="email" v-model="email" /><br />
     <label for="username">User Name</label><br />
     <input type="text" v-model="userName" /><br />
     <label for="username">Password</label><br />
     <input type="password" v-model="password" /><br />
+    <div>
+      <small v-if="errorMsg" style="color: red">{{ errorMsg }}</small>
+    </div>
     <select style="margin: 20px 0px">
       <option value="">Please Select Role</option>
       <option value="dev">Developer</option>
@@ -17,7 +20,9 @@
     <!-- <button>Reset</button> -->
 
     <div v-for="skill in skills" :key="skill">
-      <p>{{ skill }} &#10060;</p>
+      <p>
+        {{ skill }} <span class="cross" @click="delSkill(skill)">&#10060;</span>
+      </p>
     </div>
     <br />
     <!-- <button :v-model="(skills = '')">Reset</button> -->
@@ -26,6 +31,7 @@
       <input type="checkbox" v-model="accept" />
       <label style="margin-left: 5px" for="">accept terms</label><br />
     </div>
+    <button>Create</button>
     <!-- <div>
       <div>
         <label for="">name - 1</label>
@@ -56,14 +62,34 @@ export default {
       names: [],
       skills: [],
       skill: "",
+      errorMsg: "",
     };
   },
   methods: {
+    // addSkill(e) {
+    //   if (e.key === " ") {
+    //     this.skills.push(this.skill);
+    //     this.skill = "";
+    //   }
+    // },
     addSkill(e) {
-      if (e.key === " ") {
-        this.skills.push(this.skill);
+      if (e.key === " " && this.skill.trim() !== "") {
+        this.skills.push(this.skill.trim());
         this.skill = "";
       }
+    },
+    delSkill(skill) {
+      // this.skill;
+      // console.log(skill);
+      this.skills = this.skills.filter((loopSkill) => loopSkill != skill);
+    },
+    submit() {
+      if (this.password.length < 8) {
+        this.errorMsg = "password must be atleast 8 characters";
+      }
+      //  else if (this.password != ) {
+      //   this.errorMsg = "use strinng";
+      // }
     },
   },
 };
@@ -72,5 +98,8 @@ export default {
 <style>
 * {
   color: black;
+}
+.cross {
+  cursor: pointer;
 }
 </style>
